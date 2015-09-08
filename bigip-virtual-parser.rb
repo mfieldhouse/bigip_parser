88b6e7bb0020f93e00b60cb933a86fc5b5f3b62b
Matthew Fieldhouse <fieldhouse@gmail.com>
2015-09-09 00:35:52 +0100

    Parse virtual server destination as :destination

--- bigip-virtual-parser.rb
+++ bigip-virtual-parser.rb
@@ -8,20 +8,21 @@ class QuickParser < Parslet::Parser
   rule(:config)           { (virtual | ignore).repeat }
 
   rule(:lines)            { line.repeat }
-  rule(:line)             { (mask | string.as(:generic_option)) >> newline >> space? }
+  rule(:line)             { (mask | destination | string.as(:generic_option)) >> newline >> space? }
   rule(:newline)          { str("\n") }
   rule(:space)            { match('\s').repeat(1) }
   rule(:space?)           { space.maybe }
 
   rule(:string)           { (word >> str(" ").maybe).repeat(1)}
-  rule(:word)             { match('[\w!-:]').repeat(1) >> str(" ").maybe }
-  rule(:generic_line)     { space? >> (match('[\w!-:{}]').repeat(1) >> str(" ").maybe).repeat(1)}
+  rule(:word)             { match('[\w!-:=]').repeat(1) >> str(" ").maybe }
+  rule(:generic_line)     { space? >> (match('[\w!-:{}=]').repeat(1) >> str(" ").maybe).repeat(1)}
 
   rule(:ignore)           { (str('virtual').absent? >> generic_line.as(:generic_line) >> newline.maybe).repeat(1) }
 
   rule(:virtual)          { (str('virtual ') >> word.as(:name) >> 
                             space? >> str("{") >> space >> lines >> str("}")).as(:virtual_server) >> newline.maybe }
   rule(:mask)             { (str('mask ') >> string.as(:mask)) }
+  rule(:destination)      { (str('destination ') >> string.as(:destination)) }
   
   
 end

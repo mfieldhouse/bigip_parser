78466489f35fd65466ebd6b2fa5babf7c1678a25
Matthew Fieldhouse <fieldhouse@gmail.com>
2015-09-08 22:10:43 +0100

    Initial commit

---
+++ generic-parser.rb
@@ -0,0 +1,36 @@
+require 'parslet'
+require 'parslet/convenience'
+require 'pp'
+
+class QuickParser < Parslet::Parser
+  root(:lines)
+
+  rule(:lines)            { line.repeat }
+  rule(:line)             { (test.as(:test) | mask | string.as(:generic_line)) >> newline >> space? }
+  rule(:mask)             { (str('mask ') >> string.as(:mask)) }
+  rule(:newline)          { str("\n") }
+
+  rule(:string)           { (word >> str(" ").maybe).repeat(1)}
+  rule(:word)             { match('[\w!-:]').repeat(1) >> str(" ").maybe }
+  rule(:test)             { str('test') }
+  
+  rule(:space)            { match('\s').repeat(1) }
+  rule(:space?)           { space.maybe }
+end
+
+test_string = <<-END
+test
+two this is my line
+
+mask 255.255.255.0
+
+after the spaces
+
+
+theres a 
+test
+END
+
+pp test_string
+pp QuickParser.new.parse_with_debug(test_string)
+

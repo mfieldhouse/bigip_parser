5a7d671beb1359a176c12c5a72fc061557c1639c
Matthew Fieldhouse <fieldhouse@gmail.com>
2015-09-08 23:01:18 +0100

    Add code to parse single virtual server

--- generic-parser.rb
+++ generic-parser.rb
@@ -3,24 +3,34 @@ require 'parslet/convenience'
 require 'pp'
 
 class QuickParser < Parslet::Parser
-  root(:lines)
+  root(:virtual)
 
   rule(:lines)            { line.repeat }
-  rule(:line)             { (mask | string.as(:generic_line)) >> newline >> space? }
-  rule(:mask)             { (str('mask ') >> string.as(:mask)) }
+  rule(:line)             { (mask | string.as(:generic_option)) >> newline >> space? }
   rule(:newline)          { str("\n") }
+  rule(:space)            { match('\s').repeat(1) }
+  rule(:space?)           { space.maybe }
 
   rule(:string)           { (word >> str(" ").maybe).repeat(1)}
   rule(:word)             { match('[\w!-:]').repeat(1) >> str(" ").maybe }
+
+  rule(:virtual)          { str('virtual ') >> word.as(:virtual_name) >> 
+                            space? >> str("{") >> space >> lines >> str('}') >> newline }
+  rule(:mask)             { (str('mask ') >> string.as(:mask)) }
   
-  rule(:space)            { match('\s').repeat(1) }
-  rule(:space?)           { space.maybe }
+  
 end
 
 test_string = <<-END
-configuration one
-mask 255.255.255.0
-configuration two
+virtual onephonebookapisaml.prod.http {
+   destination 10.120.13.235:http
+   snatpool onephonebookapisaml.prod.http.snatpool
+   ip protocol tcp
+   profile fastL4
+   mask 255.255.255.0
+   persist source_addr_20mins
+   pool onephonebookapisaml.prod.http.pool
+}
 END
 
 pp test_string

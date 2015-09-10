58436c306212dcd8f983184c5b32d335a4993749
Matthew Fieldhouse <fieldhouse@gmail.com>
2015-09-11 00:02:22 +0100

    Complete output of parsed data to an array

--- bigip-virtual-parser.rb
+++ bigip-virtual-parser.rb
@@ -15,12 +15,13 @@ class BIGIP_v9_Parser < Parslet::Parser
                             space? >> str("{") >> space >> lines >> str("}")) >> newline.maybe }
 
   rule(:lines)            { line.repeat }
-  rule(:line)             { (mask | destination | ip_protocol | disabled | ip_forward | string.as(:generic_option)) >> newline >> space? }
+  rule(:line)             { (mask | destination | ip_protocol | disabled | ip_forward | pool | string.as(:generic_option)) >> newline >> space? }
   rule(:mask)             { (str('mask ') >> string.as(:mask)) }
-  rule(:ip_protocol)      { (str('ip protocol ') >> string.as(:ip_protocol)) }
   rule(:destination)      { (str('destination ') >> string.as(:destination)) }
-  rule(:disabled)         { str('disable').as(:disabled) }
+  rule(:pool)             { (str('pool ') >> string.as(:pool)) }
   rule(:ip_forward)       { str('ip forward').as(:ip_forward) }
+  rule(:ip_protocol)      { (str('ip protocol ') >> string.as(:ip_protocol)) }
+  rule(:disabled)         { str('disable').as(:disabled) }
 
   rule(:ignore)           { (str('virtual').absent? >> any.as(:generic_line) >> newline.maybe).repeat(1) }
 
@@ -33,36 +34,74 @@ class BIGIP_v9_Parser < Parslet::Parser
 end
 
 class Virtual_Parser
-  attr_accessor :config, :parse, :count
+  attr_accessor :config, :parse, :name
 
   def initialize(config_filename)
     @config = File.read(config_filename)
   end
 
   def parse
-    @parse = BIGIP_v9_Parser.new.parse_with_debug(@config)
+    @vips = BIGIP_v9_Parser.new.parse_with_debug(@config)
+    @vips = @vips.extend Hashie::Extensions::DeepFind
+    self
   end
 
   def count
-    @count = @parse.count
+    @count = @vips.count
   end
 
-  def name
-    vips = self.parse.map { |vip| vip.extend Hashie::Extensions::DeepFind }
-    vips.map { |x| x.deep_find :name }
+  def name  
+    @vips.deep_find(:name).to_s.gsub(' ', '')
   end
 
-  def destination
-    vips = self.parse.map { |vip| vip.extend Hashie::Extensions::DeepFind }
-    vips.map { |x| x.deep_find :destination }
+  def ip
+    dest = @vips.deep_find :destination
+    dest.to_s.split(':')[0]
+  end
+
+  def mask
+    mask = @vips.deep_find(:mask).to_s
+    if mask.empty?
+      "255.255.255.255"
+    else
+      mask
+    end
+  end
+
+  def port
+    dest = @vips.deep_find :destination
+    dest.to_s.split(':')[1]
+  end
+
+  def type
+    ip_forward = @vips.deep_find(:ip_forward).to_s
+    ip_forward.empty? ? "RESOURCE_TYPE_POOL" : "RESOURCE_TYPE_IP_FORWARDING"
+  end
+
+  def protocol
+    protocol = @vips.deep_find(:ip_protocol).to_s
+    if protocol == "tcp"
+      "PROTOCOL_TCP"
+    elsif protocol == "udp"
+      "PROTOCOL_UDP"
+    elsif protocol.empty?
+      "PROTOCOL_ANY"
+    end
+  end
+
+  def state
+    state = @vips.deep_find(:disabled).to_s
+    state.empty? ? "ENABLED_STATUS_ENABLED" : "ENABLED_STATUS_DISABLED"
+  end
+
+  def build
+    [parse.name, parse.ip, parse.mask, parse.port, parse.type, parse.protocol, parse.state]
   end
 end
 
 config = Virtual_Parser.new('sample-config.txt')
-puts config.name
-puts config.destination
+pp config.build
 
+# Access the virtual server name
+# pp config.parse[0][:virtual_server][0][:name].class
 
-
-# puts config.destination
-# puts config.count

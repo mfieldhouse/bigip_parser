593917cec51ca3dc729804d15da319043637f9ff
Matthew Fieldhouse <fieldhouse@gmail.com>
2015-10-09 17:18:20 +0100

    Clean up files

--- bin/example-methods.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/bigip-parser'
-
-config.parse_virtuals
-config.parse_snatpools
-config.parse_pools
-
-config.parse_virtuals.count
--- bin/pool-member-data.rb
+++ bin/pool-member-data.rb
@@ -1,10 +1,10 @@
 require_relative '../lib/bigip-parser'
 
-config    = BIGIP_Audit.new('../input/test.conf')
+config    = BIGIP_Audit.new('../input/LDVSF4CS04_v9_bigip.conf')
 config.parse_virtuals
 config.parse_pools
 config.parse_snatpools
-pp config.build
+config.build
 
 output_filename = "../output/output.csv"
 output_file     = File.open(output_filename, "w")
